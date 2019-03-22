b8fae9e1404d486e811598909519916263c2b4fa
Matias Gravante <matiasgravante@gmail.com>
2019-03-21 21:28:33 -0300

    segundo commit, agregue archivo y modifique main

--- main.c
+++ main.c
@@ -15,21 +15,22 @@ do {
 printf("Ingrese un valor (termina cuando ingresa '-1'): ");
 scanf("%f", &valor);
 if (contador==0){
-max=valor;
-min=valor;
-}else{
-if (valor>max){
-max=valor;
-} else if (valor<min){
-min=valor;
+    max=valor;
+    min=valor;
 }
-
+else{
+    if (valor>max){
+        max=valor;
+}
+    else if (valor<min){
+        min=valor;
+        }
 }
 resultado= suma(resultado, valor);
 contador++;
 }
 while (valor!= (-1));
-printf(" Resultado: %.2f \n Max: %.2f \n Min: %.2f", resultado, max, min);
+    printf(" Resultado: %.2f \n Max: %.2f \n Min: %.2f", resultado, max, min);
     return 0;
 }
 float suma(float primerValor, float segundoValor){
